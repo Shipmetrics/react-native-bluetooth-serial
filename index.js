80c3ac4d679f9408388a388286b0863d05445595
Leon <K-Leon@users.noreply.github.com>
2024-10-28 15:01:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 const ReactNative = require('react-native')
 const { Buffer } = require('buffer')
-const { NativeModules, DeviceEventEmitter } = ReactNative
+const { NativeModules, DeviceEventEmitter, NativeEventEmitter } = ReactNative
 const BluetoothSerial = NativeModules.BluetoothSerial
 
-/**
- * Listen for available events
- * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
- * @param  {Function} handler Event handler
- */
-BluetoothSerial.on = (eventName, handler) => {
-  DeviceEventEmitter.addListener(eventName, handler)
-}
+const BluetoothSerialEmitter = new NativeEventEmitter(BluetoothSerial)
 
-/**
- * Stop listening for event
- * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
- * @param  {Function} handler Event handler
- */
-BluetoothSerial.removeListener = (eventName, handler) => {
-  DeviceEventEmitter.removeListener(eventName, handler)
-}
-
-/**
- * Write data to device, you can pass string or buffer,
- * We must convert to base64 in RN there is no way to pass buffer directly
- * @param  {Buffer|String} data
- * @return {Promise<Boolean>}
- */
-BluetoothSerial.write = (data) => {
-  if (typeof data === 'string') {
-    data = new Buffer(data)
+class BluetoothManager {
+  /**
+   * Add listener for available events
+   * @param  {String} eventName Name of event, one of connectionSuccess, connectionLost, data, rawData
+   * @param  {Function} handler Event handler
+   */
+  static on(eventName, handler) {
+    BluetoothSerialEmitter.addListener(eventName, handler)
   }
-  return BluetoothSerial.writeToDevice(data.toString('base64'))
+
+  /**
+   * Remove listener for an event
+   * @param  {String} eventName Name of event, one of connectionSuccess, connectionLost, data, rawData
+   * @param  {Function} handler Event handler
+   */
+  static removeListener(eventName, handler) {
+    BluetoothSerialEmitter.removeListener(eventName, handler)
+  }
+
+  /**
+   * Write data to device, accepts string or buffer.
+   * Converts data to base64, as React Native does not directly support buffers.
+   * @param  {Buffer|String} data
+   * @return {Promise<Boolean>}
+   */
+  static async write(data) {
+    if (typeof data === 'string') {
+      data = Buffer.from(data)
+    }
+    return BluetoothSerial.writeToDevice(data.toString('base64'))
+  }
 }
 
-module.exports = BluetoothSerial
+module.exports = BluetoothManager
